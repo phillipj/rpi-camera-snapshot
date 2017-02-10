68fcfeac0893e3e0d02e0889b1e181b10d4c078b
Phillip Johnsen <johphi@gmail.com>
2017-02-10 18:30:24 +0100

    feat: implement /photo endpoint with example photo
    Also moved the ./index.html to ./public/index.html.

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,16 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+const halfASecond = 1 * 500;
 
 let proc;
 
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+app.get('/photo', (req, res) => {
+  setTimeout(() => {
+    res.json({ src: 'photos/example.jpg' });
+  }, halfASecond);
 });
 
 http.createServer(app).listen(3000, () => console.log('listening on *:3000'));
