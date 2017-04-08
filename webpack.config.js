cdb3770c3e7ab73b764a1d4c2a411475c8ddebc2
Phillip Johnsen <johphi@gmail.com>
2017-04-08 21:07:37 +0200

    refactor: replace elm-make & elm-live with webpack & elm-loader

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: './frontend/index.js',
+  output: {
+    filename: 'compiled.min.js',
+    path: path.resolve(__dirname, 'public/build')
+  },
+  module: {
+    loaders: [{
+      test: /\.elm$/,
+      exclude: [/elm-stuff/, /node_modules/],
+      loader: 'elm-webpack-loader'
+    }]
+  },
+  devServer: {
+    publicPath: '/build/',
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+  }
+}
